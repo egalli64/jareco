eba5f94c47d8063e85aeb624acfa5429a550648e
Emanuele <egalli64@gmail.com>
2023-03-21 11:23:35 +0100

    added from(), removed range()

--- src/main/java/com/example/jareco/s03/SomePublisherFactories.java
+++ src/main/java/com/example/jareco/s03/SomePublisherFactories.java
@@ -22,67 +22,45 @@ public class SomePublisherFactories {
     private static final Logger log = LoggerFactory.getLogger(SomePublisherFactories.class);
 
     // Source of dogs for the publishers below
-    private static final Dog[] arDogs = { new Dog(42, "Tom"), new Dog(12, "Bob"), new Dog(18, "Kim") };
+    private static final Dog[] dogs = { new Dog(42, "Tom"), new Dog(12, "Bob"), new Dog(18, "Kim") };
 
     public static void main(String[] args) {
-        log.trace("Enter");
-
         // Flux::just() creates a Flux that emits the items and then completes
-        Flux<Dog> f1 = Flux.just(arDogs[0], arDogs[2]);
-
-        log.trace("A flux just for Tom and Kim");
-        f1.subscribe(System.out::println);
+        log.trace("A Flux<Dog> just for Tom and Kim");
+        Flux.just(dogs[0], dogs[2]).subscribe(System.out::println);
 
         // Mono::just() creates a Mono that emits the item and then completes
-        Mono<Dog> m1 = Mono.just(arDogs[0]);
-
-        log.trace("A mono just for Tom");
-        m1.subscribe(System.out::println);
+        log.trace("A Mono<Dog> just for Tom");
+        Mono.just(dogs[0]).subscribe(System.out::println);
 
         // Flux::empty() creates a Flux that completes without emitting any item
-        Flux<Dog> f2 = Flux.empty();
-
-        log.trace("An empty flux");
-        f2.subscribe(System.out::println);
+        log.trace("An empty Flux<Object>");
+        Flux.empty().subscribe(System.out::println);
 
         // Flux::empty() creates a Mono that completes without emitting any item
-        Mono<Dog> m2 = Mono.empty();
-
-        log.trace("An empty mono");
-        m2.subscribe(System.out::println);
+        log.trace("An empty Mono<Object>");
+        Mono.empty().subscribe(System.out::println);
 
         // Mono::fromCallable() a supplier generate the item at runtime
-        Mono<Dog> m3 = Mono.fromCallable(() -> arDogs[1]);
-
-        log.trace("A mono for Bob from callable");
-        m3.subscribe(System.out::println);
+        log.trace("A Mono<Dog> for Bob from callable");
+        Mono<Dog> callDog = Mono.fromCallable(() -> dogs[1]);
+        callDog.subscribe(System.out::println);
 
         // Flux::fromArray() an array is used as source
-        Flux<Dog> f3 = Flux.fromArray(arDogs);
-
-        log.trace("A flux for all dogs in an array");
-        f3.subscribe(System.out::println);
+        log.trace("A Flux<Dog> based on an array");
+        Flux.fromArray(dogs).subscribe(System.out::println);
 
         // Flux::fromIterable() an iterable is used as source
-        List<Dog> liDogs = Arrays.asList(arDogs);
-        Flux<Dog> f4 = Flux.fromIterable(liDogs);
-
-        log.trace("A flux for all dogs in an iterable");
-        f4.subscribe(System.out::println);
+        log.trace("A Flux<Dog> based on an iterable");
+        List<Dog> liDogs = Arrays.asList(dogs);
+        Flux.fromIterable(liDogs).subscribe(System.out::println);
 
         // Flux::fromStream() a stream is used as source
+        log.trace("A Flux<Dog> based on a stream");
         Stream<Dog> stDogs = liDogs.stream();
-        Flux<Dog> f5 = Flux.fromStream(stDogs);
+        Flux.fromStream(stDogs).subscribe(System.out::println);
 
-        log.trace("A flux for all dogs in a stream");
-        f5.subscribe(System.out::println);
-
-        // Flux::range() from start, count integers are generated
-        Flux<Integer> f6 = Flux.range(42, 3);
-
-        log.trace("A flux for a range");
-        f6.subscribe(System.out::println);
-
-        log.trace("Exit");
+        log.trace("A Flux<Dog> based on a Publisher");
+        Flux.from(callDog).subscribe(System.out::println);
     }
 }
